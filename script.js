98de3f4187cf19d861bd9fc5cbcf4feeefe58c79
Lara <debbiegterra@gmail.com>
2021-09-02 15:20:49 +0100

    question 3 answer

--- script.js
+++ script.js
@@ -44,11 +44,14 @@ const isAdult = people.some(person => {
     return currentYear - person.year >= 19;
     });
     console.log(allIsAdult);
-    
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
-
+    const findId = comments.find(function(id) {
+        return id.id === 823423;
+    });
+    console.log(findId);
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
