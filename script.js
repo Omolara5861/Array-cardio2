56c6192aaed8ab411752de83b5691e1b5c59259d
Lara <debbiegterra@gmail.com>
2021-09-02 15:14:39 +0100

    shorter code for answer2

--- script.js
+++ script.js
@@ -31,15 +31,20 @@ const isAdult = people.some(person => {
     console.log(isAdult);
 
   // Array.prototype.every() // is everyone 19 or older?
-    const  allIsAdult = people.every(function(person) {
-        const currentYear = new Date().getFullYear();
-        if(currentYear - person.year >= 19) {
-            return true;
-        }
+    // const  allIsAdult = people.every(function(person) {
+    //     const currentYear = new Date().getFullYear();
+    //     if(currentYear - person.year >= 19) {
+    //         return true;
+    //     }
+    // });
+    // console.log(allIsAdult);
+    // shorter implementation
+    const  allIsAdult = people.every(person => {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.year >= 19;
     });
     console.log(allIsAdult);
-
-
+    
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
